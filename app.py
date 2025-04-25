86ba71de7e0a13a21fef3544d6bf339fa308f8aa
alikarimi2000 <karimyai46@gmail.com>
2025-04-26 02:09:25 +0330

    12

--- app.py
+++ app.py
@@ -51,6 +51,5 @@ def register():
 
     return jsonify({'message': 'Registration successful!'}), 200
 
-# ---------- اجرای برنامه ----------
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
